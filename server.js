8882fe9a64c91c672f08bdf33117681ad2a46916
Kommisetti Abhinav <23eg105t34@anurag.edu.in>
2026-02-27 23:51:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,4 +16,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/todos", require("./routes/todo"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
